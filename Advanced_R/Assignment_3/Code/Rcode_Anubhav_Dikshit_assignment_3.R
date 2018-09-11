1d8b7e9e44f1f1c1160077c31dd83b914c21325f
Anubhav Dikshit <anubhav-dikshit@users.noreply.github.com>
2018-09-12 00:14:27 +0200

    Added the Assignment three

--- Advanced_R/Assignment_3/Code/Rcode_Anubhav_Dikshit_assignment_3.R
+++ Advanced_R/Assignment_3/Code/Rcode_Anubhav_Dikshit_assignment_3.R
@@ -10,6 +10,7 @@ gc() #Garbage collector
 name <- "Anubhav Dikshit"
 liuid <- "anudi287"
 lab_path <- (path = "https://raw.githubusercontent.com/STIMALiU/AdvRCourse/master/Labs/Tests/lab3.yml")
+setwd("C:/Users/anubh/OneDrive/Documents/GitHub/LiU/LiU-2018/Advanced_R/Assignment_3")
 
 #**********************************************************************************************************************************#
 #*                                            LOADING THE LIBRARY IF NOT FOUND INSTALL                                            *#
@@ -26,9 +27,20 @@ set_assignment(lab_path)
 # 1.1.1 euclidean()
 
 euclidean <- function(a,b){
-  
+if(a == 0 | b == 0 | is.na(a) | is.na(b)){stop("incorrect inputs")}
+if(a > b){
+  temp <- b
+  b <- a
+  a <- temp
 }
+    r <- b%%a
+    return(ifelse(r, euclidean(a, r), a))
+  }  
 
 
-
-
+# create this function and run the following
+usethis::create_package("~/Rassignment3")
+devtools::load_all(".")
+usethis::use_readme_md()
+usethis::use_test()
+covr::report()
--- Visualization/Assignment_1/Code/Rcode_for_assignment_1_visualization.R
+++ Visualization/Assignment_1/Code/Rcode_for_assignment_1_visualization.R
@@ -15,7 +15,7 @@ setwd("C:/Users/anubh/OneDrive/Documents/GitHub/LiU/LiU-2018/Visualization/Assig
 #**********************************************************************************************************************************#
 
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(ggplot2, readr, gridExtra, data.table)
+pacman::p_load(ggplot2, readr, gridExtra, data.table, plotly, shiny)
 
 #**********************************************************************************************************************************#
 #*                                                         LOADING THE INPUTS                                                     *#
@@ -90,8 +90,147 @@ grid.arrange(p1, p2, p3, p4, p5, p6, p7, p9) # facility_per does not contain any
 plot(p7) # nurse
 plot(p9) # risk
 
-ggplot(data = data_senic, aes(x = avg_nurses, y = avg_risk)) + geom_point(aes(color = avg_beds)) + geom_count()
+ggplot(data = data_senic, aes(x = avg_nurses, y = avg_risk)) + geom_point(aes(color = avg_beds))
+
+#**********************************************************************************************************************************#
+#*                                      CONVERTING GGPLOT INTO PLOTLY                                                             *#
+#**********************************************************************************************************************************#
+
+# zooming and subseting
+ggplotly(p9)
+
+#**********************************************************************************************************************************#
+#*                                      PIPLING AND  PLOTLY                                                                       *#
+#**********************************************************************************************************************************#
+
+
+# NEED TO COMPLETE FROM HERE
+# zooming and subseting
+ggplotly(p9)
+
+data_senic %>% select(ID, avg_risk) %>% plot_ly(x = ~avg_risk, type = "histogram") %>% add_markers(y = ~avg_risk, showlegend = FALSE)
+
+#**********************************************************************************************************************************#
+#*                                      SHINY APP FOR PLOTS                                                                        *#
+#**********************************************************************************************************************************#
+
+# all plots, using a for loop would be easier, will think of writing this under a function
+p1 <- ggplot(data=data_senic_numeric, aes(x=length_of_stay)) + geom_density() + geom_point(data = data_senic_numeric[quant_cal(data_senic_numeric$length_of_stay), c("length_of_stay")], aes(y = 0, x = length_of_stay), shape=18, size=2) 
+p2 <- ggplot(data=data_senic_numeric, aes(x=avg_age)) + geom_density() + geom_point(data = data_senic_numeric[quant_cal(data_senic_numeric$avg_age), c("avg_age")], aes(y = 0, x = avg_age), shape=18, size=2) 
+p3 <- ggplot(data=data_senic_numeric, aes(x=culturing_ratio)) + geom_density() + geom_point(data = data_senic_numeric[quant_cal(data_senic_numeric$culturing_ratio), c("culturing_ratio")], aes(y = 0, x = culturing_ratio), shape=18, size=2) 
+p4 <- ggplot(data=data_senic_numeric, aes(x=chest_xray_ratio)) + geom_density() + geom_point(data = data_senic_numeric[quant_cal(data_senic_numeric$chest_xray_ratio), c("chest_xray_ratio")], aes(y = 0, x = chest_xray_ratio), shape=18, size=2) 
+p5 <- ggplot(data=data_senic_numeric, aes(x=avg_beds)) + geom_density() + geom_point(data = data_senic_numeric[quant_cal(data_senic_numeric$avg_beds), c("avg_beds")], aes(y = 0, x = avg_beds), shape=18, size=2) 
+p6 <- ggplot(data=data_senic_numeric, aes(x=avg_daily_census)) + geom_density() + geom_point(data = data_senic_numeric[quant_cal(data_senic_numeric$avg_daily_census), c("avg_daily_census")], aes(y = 0, x = avg_daily_census), shape=18, size=2) 
+p7 <- ggplot(data=data_senic_numeric, aes(x=avg_nurses)) + geom_density() + geom_point(data = data_senic_numeric[quant_cal(data_senic_numeric$avg_nurses), c("avg_nurses")], aes(y = 0, x = avg_nurses), shape=18, size=2) 
+p8 <- ggplot(data=data_senic_numeric, aes(x=facility_per)) + geom_density() + geom_point(data = data_senic_numeric[quant_cal(data_senic_numeric$facility_per), c("facility_per")], aes(y = 0, x = facility_per), shape=18, size=2) 
+p9 <- ggplot(data=data_senic_numeric, aes(x=avg_risk)) + geom_density() + geom_point(data = data_senic_numeric[quant_cal(data_senic_numeric$avg_risk), c("avg_risk")], aes(y = 0, x = avg_risk), shape=18, size=2) 
 
 
 
 
+
+
+ui <- fluidPage(
+  sliderInput(inputId="ws", label="Choose bandwidth size", value=0.01, min=0.1, max=1),
+  checkboxGroupInput("variable", "Variables to plot:",
+                     c("Cylinders" = "cyl",
+                       "Transmission" = "am",
+                       "Gears" = "gear")),,
+  plotOutput("densPlot")
+)
+
+server <- function(input, output) {
+  
+  output$densPlot <- renderPlot({
+    ggplot(iris, aes(x=Sepal.Length,fill=Species))+
+      stat_density(alpha=0.8, bw=input$ws, position="identity")
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
+
+library(shiny)
+library(ggplot2)
+library(data.table)
+
+
+
+# Assignment 2.1
+
+data_senic <- read_table2("Input/SENIC.txt", col_names = FALSE)
+
+# Fixing the column names
+
+setnames(data_senic, old = c("X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "X9", "X10", "X11", "X12"), 
+         new = c("ID", "length_of_stay", "avg_age", "avg_risk", "culturing_ratio", "chest_xray_ratio",
+                 "avg_beds", "school_affil", "region", "avg_daily_census", "avg_nurses", "facility_per"))
+
+# Fixing the datatypes
+
+data_senic$school_affil <- factor(data_senic$school_affil, levels = c(1, 2),labels = c("Yes", "No"))
+data_senic$region <- factor(data_senic$region, levels = c(1, 2, 3, 4),labels = c("NE", "NC", "S", "W"))
+
+#**********************************************************************************************************************************#
+#*                                      CREATING FUNCTION TO CALCULATE THE QUALITLE HIGHLIGHT THE OUTLIERS                        *#
+#**********************************************************************************************************************************#
+
+quant_cal <- function(x){
+  #x <-  mtcars$hp
+  q1 <- as.numeric(quantile(x, probs = c(0.25)))
+  q3 <- as.numeric(quantile(x, probs = c(0.75)))
+  iqr <- q3-q1
+  upper_value <- q3+(1.5*iqr)
+  lower_value <- q1-(1.5*iqr)
+  outliers_index <- which(x < lower_value | x > upper_value)
+  #which(x %in% boxplot(x)$out) # must be equal to above
+  return(outliers_index)
+}
+
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Hello Shiny!"),
+  
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("Density",
+                  "Bandwidth:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    checkboxInput("checkbox", label = "Choice A", value = TRUE),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  # Expression that generates a histogram. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should be automatically
+  #     re-executed when inputs change
+  #  2) Its output type is a plot
+  
+  output$distPlot <- renderPlot({
+    x    <- data_senic[[]]  # Old Faithful Geyser data
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+  
+})
