7609347e3566f76d75dac3a4da1b3bd0913cc7de
Anubhav Dikshit <anubhav.dikshit@live.in>
2018-09-13 09:24:22 +0200

    Added example function

--- Advanced_R/Assignment_3/Code/Rcode_Anubhav_Dikshit_assignment_3.R
+++ Advanced_R/Assignment_3/Code/Rcode_Anubhav_Dikshit_assignment_3.R
@@ -85,7 +85,11 @@ dijkstra_ext <- function(graph, init_node) {
 #' @return returns a vector with distance to all other nodes
 #' @export
 #'
-#' @examples dijkstra_int(wiki_graph, 1)
+#' @examples wiki_graph <- data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+#'  v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+#'  w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+#'  dijkstra_int(wiki_graph, 1)
+
 dijkstra_int <- function(df, row) {
   if(is.data.frame(df) & is.numeric(row)){
     n <- length(unique(df[,1]))
@@ -181,6 +185,20 @@ usethis::use_readme_md()
 
 # dijkstra_int(wiki_graph, 1)
 # dijkstra_ext(wiki_graph, 1)
+
+#' #' A graph dataset is present here
+#' #'
+#' #' A dataset containing the source, destination and weight of each node
+#' #'
+#' #' @format A data frame with 18 rows and 3 variables:
+#' #' \describe{
+#' #'   \item{v1}{source node}
+#' #'   \item{v2}{destination node}
+#' #'   \item{w}{weight or distance}
+#' #'   ...
+#' #' }
+#' "wikigraph"
+
 # euclidean(100, 1000)
 
 usethis::use_test("my-test")
