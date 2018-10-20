c284de2c0b46964398115be19047d75b8c776024
Anubhav Dikshit <anubhav.dikshit@live.in>
2018-10-20 13:48:38 +0200

    Added html code

--- Visualization/Hari_Visualization/All labs/Cheat_book_convert_pdf.R
+++
@@ -1,3 +0,0 @@
-library(webshot)
-
-rmdshot("C:/Users/anubh/OneDrive/Documents/GitHub/LiU/LiU-2018/Visualization/Hari Visualization/All labs/Cheat_book.Rmd", "Cheat_book.pdf")
---
+++ Visualization/Hari_Visualization/All_labs/Cheat_book_convert_pdf.R
@@ -0,0 +1,5 @@
+library(webshot)
+library(rmarkdown)
+
+rmdshot("C:/Users/anubh/OneDrive/Documents/GitHub/LiU/LiU_2018/Visualization/Hari_Visualization/All_labs/Cheat_book.Rmd", 
+        file = "Cheat_book.pdf",vwidth = 800)
